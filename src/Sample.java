7c2c111e3c496e332b35e8712166a2fd06f8f016
EMD-572 <EMD-572@EMD572>
2026-06-15 10:49:45 +0900

    初回コミット

---
+++ src/Sample.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Sample {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Swing ウィンドウ");
+        frame.setSize(400, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.add(new JLabel("Swing が使えるよ！"));
+        frame.setVisible(true);
+    }
+}
